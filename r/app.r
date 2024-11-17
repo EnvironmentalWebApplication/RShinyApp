947585570a0f94528372902f7ea5b66f4e722d28
Josh Parent <joshuamparent@gmail.com>
2024-11-16 23:51:05 -0500

    Replaced old data with daily average & sub daily data sets.  Problems exist with the data though.  Users can select daily average or sub daily on relevant graphs.

--- r/app.r
+++ r/app.r
@@ -3,7 +3,13 @@ library(ggplot2)
 library(colorRamps)
 
 # Load Lake Data
-lakeData <- read.csv("./r/data/Cleaned_LongPond_08082024.csv")
+# lakeData <- read.csv("./r/data/Cleaned_LongPond_08082024.csv") OLD DATA
+dailyLakeData <- read.table("./r/data/DailyAverage.txt", header = TRUE, sep = "\t")
+subDailyLakeData <- read.table("./r/data/SubDailyAverage.txt", header = TRUE, sep = "\t")
+
+# Standardize column names
+colnames(dailyLakeData) <- c("sensorType", "meter", "date", "value", "STD", "var", "n")
+colnames(subDailyLakeData) <- c("date", "value", "meter", "sensorType")
 
 # Define UI
 ui <- fluidPage(
@@ -19,7 +25,8 @@ ui <- fluidPage(
                  selectInput(
                    "graphSelect",
                    "Select Graph",
-                   choices = c("Heatmap", "DO at Depth", "Temperature at Depth")
+                   choices = c("Heatmap", "DO at Depth", "Temperature at Depth"),
+                   selected = "DO at Depth"
                  ),
                  uiOutput("graphParameters"),
                  uiOutput("dateParameters")
@@ -52,35 +59,55 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
   #Filter the data into sensor types
-  doData <- lakeData[lakeData$sensor_type == "DO",]
-  heatData <- lakeData[lakeData$sensor_type == "Temperature",]
+  DailyDO <- dailyLakeData[dailyLakeData$sensorType == "DO",]
+  DailyHeat <- dailyLakeData[dailyLakeData$sensorType == "Temp",]
+  subDailyDO <- subDailyLakeData[subDailyLakeData$sensorType == "DO",]
+  subDailyHeat <- subDailyLakeData[subDailyLakeData$sensorType == "Temp",]
 
   #Find all depths of the different sensors
-  doDepthChoices <- sort(unique(doData$meter), decreasing = FALSE)
-  heatDepthChoices <- sort(unique(heatData$meter), decreasing = FALSE)
+  doDepthChoices <- sort(unique(DailyDO$meter), decreasing = FALSE)
+  heatDepthChoices <- sort(unique(DailyHeat$meter), decreasing = FALSE)
 
   #Format days
-  doData$date <- as.Date(doData$date, format = "%m/%d/%y")
-  heatData$date <- as.Date(heatData$date, format = "%m/%d/%y")
+  DailyDO$date <- as.Date(DailyDO$date)
+  DailyHeat$date <- as.Date(DailyHeat$date)
+  subDailyDO$date <- as.Date(subDailyDO$date)
+  subDailyHeat$date <- as.Date(subDailyHeat$date)
 
   # Graph settings (check boxes)
   output$graphParameters <- renderUI({
-      if (input$graphSelect == "Heatmap") {
+    if (input$graphSelect == "Heatmap") {
       # Space to add graph parameters to heatmap
-      }
-      else if (input$graphSelect == "DO at Depth") {
+    }
+    else if (input$graphSelect == "DO at Depth") {
+      tagList(
+        selectInput(
+          "frequencyDO",
+          "Select Display Frequency",
+          choices = c("Daily Average", "Sub Daily"),
+          selected = "Daily Average"
+        ),
         checkboxGroupInput(
-        "doDepth",
-        "Select Depths (Meters)",
-        choices = doDepthChoices,
-        selected = min(doDepthChoices),
+          "doDepth",
+          "Select Depths (Meters)",
+          choices = doDepthChoices,
+          selected = min(doDepthChoices),
         )
-      } else if (input$graphSelect == "Temperature at Depth") {
-      checkboxGroupInput(
-        "heatDepth",
-        "Select Depths (Meters)",
-        choices = heatDepthChoices,
-        selected = min(heatDepthChoices)
+      )
+    } else if (input$graphSelect == "Temperature at Depth") {
+      tagList(
+        selectInput(
+          "frequencyHeat",
+          "Select Display Frequency",
+          choices = c("Daily Average", "Sub Daily"),
+          selected = "Daily Average"
+        ),
+        checkboxGroupInput(
+          "heatDepth",
+          "Select Depths (Meters)",
+          choices = heatDepthChoices,
+          selected = min(heatDepthChoices)
+        )
       )
     }
 
@@ -92,21 +119,20 @@ server <- function(input, output) {
       dateRangeInput(
         "doDates",
         "Select Date Range",
-        start = min(doData$date),
-        end = max(doData$date),
-        min = min(doData$date),
-        max = max(doData$date),
+        start = min(DailyDO$date),
+        end = max(DailyDO$date),
+        min = min(DailyDO$date),
+        max = max(DailyDO$date),
         format = "mm/dd/yyyy"
       )
-    } else if (input$graphSelect == "Heatmap" ||
-      input$graphSelect == "Temperature at Depth") {
+    } else if (input$graphSelect == "Heatmap" || input$graphSelect == "Temperature at Depth") {
       dateRangeInput(
         "heatDates",
         "Select Date Range",
-        start = min(heatData$date),
-        end = max(heatData$date),
-        min = min(heatData$date),
-        max = max(heatData$date),
+        start = min(DailyHeat$date),
+        end = max(DailyHeat$date),
+        min = min(DailyHeat$date),
+        max = max(DailyHeat$date),
         format = "mm/dd/yyyy"
       )
     }
@@ -116,31 +142,47 @@ server <- function(input, output) {
   selectedData <- reactive({
     # DO data
     if (input$graphSelect == "Heatmap") {
-      selected <- heatData[as.Date(heatData$date) >= input$heatDates[1] &
-                             as.Date(heatData$date) <= input$heatDates[2],]
-      # Heatmap data
-    } else if (input$graphSelect == "DO at Depth") {
-      selected <- doData[doData$meter == input$doDepth &
-                           as.Date(doData$date) >= input$doDates[1] &
-                           as.Date(doData$date) <= input$doDates[2],]
-      # Heat scatterplot data
-    } else if (input$graphSelect == "Temperature at Depth") {
-      selected <- heatData[heatData$meter == input$heatDepth &
-                             as.Date(heatData$date) >= input$heatDates[1] &
-                             as.Date(heatData$date) <= input$heatDates[2],]
+      selected <- DailyHeat[as.Date(DailyHeat$date) >= input$heatDates[1] &
+                              as.Date(DailyHeat$date) <= input$heatDates[2],]
+    }
+    # Heatmap data
+    else if (input$graphSelect == "DO at Depth") {
+      if (input$frequencyDO == "Daily Average") {
+        selected <- DailyDO[DailyDO$meter %in% input$doDepth &
+                              as.Date(DailyDO$date) >= input$doDates[1] &
+                              as.Date(DailyDO$date) <= input$doDates[2],]
+      }
+      else if (input$frequencyDO == "Sub Daily") {
+        selected <- subDailyDO[subDailyDO$meter %in% input$doDepth &
+                              as.Date(subDailyDO$date) >= input$doDates[1] &
+                              as.Date(subDailyDO$date) <= input$doDates[2],]
+      }
+    }
+    # Heat scatterplot data
+    else if (input$graphSelect == "Temperature at Depth") {
+      if (input$frequencyHeat == "Daily Average") {
+        selected <- DailyHeat[DailyHeat$meter %in% input$heatDepth &
+                                as.Date(DailyHeat$date) >= input$heatDates[1] &
+                                as.Date(DailyHeat$date) <= input$heatDates[2],]
+      }
+      else if (input$frequencyHeat == "Sub Daily") {
+        selected <- subDailyHeat[subDailyHeat$meter %in% input$heatDepth &
+                                as.Date(subDailyHeat$date) >= input$heatDates[1] &
+                                as.Date(subDailyHeat$date) <= input$heatDates[2],]
+      }
     }
 
     return(selected)
   })
 
   # Manual color setting for depths
-  depthColors <- c("1" = "#f87a71", "2" = "#c39b24", "3" = "#54b321", "4" = "#05be95",
-                   "5" = "#08b4e8", "6" = "#a68bfc", "7" = "#fb66d5")
+  depthColors <- c("1.52" = "#f87a71", "2.52" = "#c39b24", "3.52" = "#54b321", "4.52" = "#05be95",
+                   "5.52" = "#08b4e8", "6.52" = "#a68bfc", "7.52" = "#fb66d5")
 
   # Graphs
   output$selectedPlot <- renderPlot({
     if (input$graphSelect == "Heatmap") {
-      ggplot(selectedData(), aes(x = date, y = meter, fill = Value)) +
+      ggplot(selectedData(), aes(x = date, y = meter, fill = value)) +
         geom_raster(interpolate = T) +
         scale_y_continuous(
           trans = "reverse",
@@ -155,7 +197,7 @@ server <- function(input, output) {
         ) +
         theme_classic()
     } else if (input$graphSelect == "DO at Depth") {
-      ggplot(selectedData(), aes(x = date, y = Value, color = factor(meter))) +
+      ggplot(selectedData(), aes(x = date, y = value, color = factor(meter))) +
         geom_point() +
         scale_color_manual(values = depthColors) +
         scale_y_continuous(breaks = seq(0, 15, by = 3),
@@ -167,7 +209,7 @@ server <- function(input, output) {
         ) +
         theme_bw()
     } else if (input$graphSelect == "Temperature at Depth") {
-      ggplot(selectedData(), aes(x = date, y = Value, color = factor(meter))) +
+      ggplot(selectedData(), aes(x = date, y = value, color = factor(meter))) +
         geom_point() +
         scale_color_manual(values = depthColors) +
         scale_y_continuous(breaks = seq(0, 30, by = 5),
