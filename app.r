89617f26b7f95606a32baca7947b650572b25762
Josh Parent <joshuamparent@gmail.com>
2025-03-20 17:41:39 -0400

    Added more comments

--- app.r
+++ app.r
@@ -5,10 +5,10 @@ library(dplyr)
 library(tidyr)
 library(ggh4x)
 
-#TODO Things left to: swap interpolated data set, swap wq data set adding nitrate
+#TODO Things left: documentation
 #TODO Meeting topics:
 
-# Load Lake Data
+# Load all data sets
 dailyLakeData <- read.table("./data/DailyAverage.txt", header = TRUE, sep = "\t")
 subDailyLakeData <- read.table("./data/SubDailyAverage.txt", header = TRUE, sep = "\t")
 YSI <- read.table("./data/ysi.txt", header = TRUE, sep = "\t")
@@ -16,7 +16,10 @@ WQ <- read.table("./data/wq.txt", header = TRUE, sep = "\t")
 WQDOC <- read.table("./data/2024_lng_DOC_for_analysis.txt", header = TRUE, sep = "\t")
 WQSecchi <- read.table("./data/secchi.txt", header = TRUE, sep = "\t")
 
-# Standardize column names
+# Standardize column names on data sets
+# If a data set is switched, check the column order.  If the order changes, update the order
+# below.  Changing a column name, not the position, won't effect the app.  If a new column
+# is added, add it below.
 colnames(dailyLakeData) <- c("sensorType", "meter", "date",
                              "value", "STD", "var", "n")
 colnames(subDailyLakeData) <- c("date", "value", "meter", "sensorType")
@@ -30,21 +33,22 @@ colnames(WQSecchi) <- c("date", "secchi")
 ui <- fluidPage(
   titlePanel(""),
 
-  # Create a single tabsetPanel to hold multiple tabPanel elements
+  # tabsetPanel to hold multiple tabPanel elements
   tabsetPanel(
 
     # High-frequency data tab
     tabPanel("High-Frequency Data",
              sidebarLayout(
                sidebarPanel(
+                 # Selection for HF graphs
                  selectInput(
                    "HFGraphSelect",
                    "Select Graph",
                    choices = c("Heatmap", "DO at Depth", "Temperature at Depth"),
-                   selected = "DO at Depth"
+                   selected = "Heatmap"
                  ),
-                 uiOutput("graphParameters"),
-                 uiOutput("dateParameters")
+                 uiOutput("HFGraphParameters"),
+                 uiOutput("HFDateParameters")
                ),
                mainPanel(
                  plotOutput("HFPlot")
@@ -56,6 +60,7 @@ ui <- fluidPage(
     tabPanel("Manual Sampling",
              sidebarLayout(
                sidebarPanel(
+                 # Selection for MS graphs
                  selectInput(
                    "msTab",
                    "Select Graph",
@@ -65,6 +70,8 @@ ui <- fluidPage(
                  uiOutput("manualSamplingParameters")
                ),
                mainPanel(
+                 # Conditional pannels based on user selected graph
+                 # Full pannel if YSI, split pannel if WQ
                  conditionalPanel(
                    condition = "input.msTab == 'YSI'",
                    plotOutput("MSPlot")
@@ -79,8 +86,7 @@ ui <- fluidPage(
                ),
              )
     ),
-    # selected = "High-Frequency Data"
-    selected = "Manual Sampling"
+    selected = "High-Frequency Data"
   )
 )
 
@@ -97,25 +103,29 @@ server <- function(input, output) {
   doDepthChoices <- sort(unique(DailyDO$meter), decreasing = FALSE)
   heatDepthChoices <- sort(unique(DailyHeat$meter), decreasing = FALSE)
 
-  #Format days
+  #Format days in the data sets
   DailyDO$date <- as.Date(DailyDO$date)
   DailyHeat$date <- as.Date(DailyHeat$date)
   subDailyDO$date <- as.POSIXct(subDailyDO$date, format = "%Y-%m-%d %H:%M:%S")
   subDailyHeat$date <- as.POSIXct(subDailyHeat$date, format = "%Y-%m-%d %H:%M:%S")
 
-  # Graph settings (check boxes)
-  output$graphParameters <- renderUI({
+  # User selection for HF graph parameters
+  output$HFGraphParameters <- renderUI({
+    # Heatmap parameter selection
     if (input$HFGraphSelect == "Heatmap") {
-      # Placeholder to add graph parameters to heatmap
+      # Placeholder to add graph parameters to heatmap if needed in the future
     }
+    # DO plot parameter selection
     else if (input$HFGraphSelect == "DO at Depth") {
       tagList(
+        # DO data frequency selection
         selectInput(
           "frequencyDO",
           "Select Display Frequency",
           choices = c("Daily Average", "Sub Daily"),
           selected = "Daily Average"
         ),
+        # DO depth selection
         checkboxGroupInput(
           "doDepth",
           "Select Depths (Meters)",
@@ -123,14 +133,17 @@ server <- function(input, output) {
           selected = c("1.52", "2.52", "3.52", "4.52", "5.52", "6.52", "7.52")
         )
       )
+    #Temperature plot parameter selection
     } else if (input$HFGraphSelect == "Temperature at Depth") {
       tagList(
+        # Heat data frequency selection
         selectInput(
           "frequencyHeat",
           "Select Display Frequency",
           choices = c("Daily Average", "Sub Daily"),
           selected = "Daily Average"
         ),
+        # Heat depth selection
         checkboxGroupInput(
           "heatDepth",
           "Select Depths (Meters)",
@@ -142,8 +155,9 @@ server <- function(input, output) {
 
   })
 
-  # Date settings (Calander)
-  output$dateParameters <- renderUI({
+  # User selection for HF data range (dates)
+  output$HFDateParameters <- renderUI({
+    #DO plot date selection
     if (input$HFGraphSelect == "DO at Depth") {
       dateRangeInput(
         "doDates",
@@ -154,6 +168,7 @@ server <- function(input, output) {
         max = max(DailyDO$date),
         format = "mm/dd/yyyy"
       )
+    # Heat plot date selection
     } else if (input$HFGraphSelect == "Temperature at Depth") {
       dateRangeInput(
         "heatDates",
@@ -164,6 +179,7 @@ server <- function(input, output) {
         max = max(DailyHeat$date),
         format = "mm/dd/yyyy"
       )
+    # Heatmap plot date selection
     } else if (input$HFGraphSelect == "Heatmap") {
       dateRangeInput(
         "heatmapDateChoices",
@@ -182,15 +198,19 @@ server <- function(input, output) {
                  "Dissolved Organic Carbon", "Soluble Reactive Phosphorus",
                  "Secchi Depth", "Ammonium", "Chlorophyll A")
 
+  #User selection for manual sampling graph parameters
   output$manualSamplingParameters <- renderUI({
+    # YSI plots
     if (input$msTab == "YSI") {
       tagList(
+        # YSI graph selection
         selectInput(
           "YSIGraphSelect",
           "Select graph",
           choices = c("Temperature", "DO", "Both"),
           selected = "Temperature"
         ),
+        # YSI date range selection
         checkboxGroupInput(
           "YSIDateSelect",
           "Select Date Range",
@@ -199,14 +219,17 @@ server <- function(input, output) {
         )
       )
     }
+    # Water quality plots
     else if (input$msTab == "Water Quality") {
       tagList(
+        # Left WQ plot selection
         selectInput(
           "leftWQSelect",
           "Select Left Graph",
           choices = WQChoices,
           selected = WQChoices[1]
         ),
+        # Right WQ plot selection
         selectInput(
           "rightWQSelect",
           "Select Right Graph",
@@ -217,18 +240,20 @@ server <- function(input, output) {
     }
   })
 
-  # Select data based on user input
+  # HF data reactive object
   selectedHF <- reactive({
     req(input$HFGraphSelect)
 
     # DO data
     if (input$HFGraphSelect == "Heatmap") {
+      # Data is filtered based on user selected dates
       selected <- DailyHeat[DailyHeat$date >= input$heatmapDateChoices[1] &
                               DailyHeat$date <= input$heatmapDateChoices[2],]
     }
     # Heatmap data
     else if (input$HFGraphSelect == "DO at Depth") {
       req(input$frequencyDO)
+      # Data is filtered based on user selected dates and depth
       if (input$frequencyDO == "Daily Average") {
         selected <- DailyDO[DailyDO$meter %in% input$doDepth &
                               DailyDO$date >= input$doDates[1] &
@@ -243,6 +268,7 @@ server <- function(input, output) {
     # Heat scatterplot data
     else if (input$HFGraphSelect == "Temperature at Depth") {
       req(input$frequencyHeat)
+      # Data is filtered based on user selected dates and depth
       if (input$frequencyHeat == "Daily Average") {
         selected <- DailyHeat[DailyHeat$meter %in% input$heatDepth &
                                 DailyHeat$date >= input$heatDates[1] &
@@ -258,12 +284,17 @@ server <- function(input, output) {
     return(selected)
   })
 
+  # MS data reactive object
   selectedMS <- reactive({
     req(input$msTab, input$YSIGraphSelect, input$YSIDateSelect)
+
+    # Only YSI has a reactive object
     if (input$YSIGraphSelect != "Both") {
+      # Temp and DO plots are filtered based on user selected dates
       selected <- YSI[YSI$date %in% input$YSIDateSelect,]
     }
     else {
+      # Combined plot is filtered based on data is reformated using pivot longer
       selected <-
         YSI %>%
           filter(date %in% input$YSIDateSelect) %>%
@@ -272,7 +303,8 @@ server <- function(input, output) {
     return(selected)
   })
 
-  # Y-axis limits for left WQ graphs
+  # Reactive object for y-axis limits for left WQ graphs
+  # Returns y-axis limits based on user selected plot
   WQYScaleLeft <- reactive({
     req(input$leftWQSelect)
 
@@ -295,7 +327,8 @@ server <- function(input, output) {
     }
   })
 
-  # Y-axis limits for right WQ graphs
+  # Reactive object for y-axis limits for right WQ graphs
+  # Returns y-axis limits based on user selected plot
   WQYScaleRight <- reactive({
     req(input$rightWQSelect)
 
@@ -318,11 +351,11 @@ server <- function(input, output) {
     }
   })
 
-  # Manual color settings for depths
+  # Manual color settings for depths in high-frequency data plots
   depthColors <- c("1.52" = "#f87a71", "2.52" = "#c39b24", "3.52" = "#54b321", "4.52" = "#05be95",
                    "5.52" = "#08b4e8", "6.52" = "#a68bfc", "7.52" = "#fb66d5")
 
-  # Manual shape settings for depths
+  # Manual shape settings for depths in high-frequency data plots
   depthShapes <- c("1.52" = 21,  # Circle
                    "2.52" = 22,  # Square
                    "3.52" = 23,  # Diamond
@@ -331,6 +364,7 @@ server <- function(input, output) {
                    "6.52" = 3,   # + sign
                    "7.52" = 25)  # Triangle point-down
 
+  # Functions that returns y-axis lables for the water quality plots
   WQYAxisLabel <- function(selectedParam) {
     if (selectedParam == "Total Phosphorus") {
       "Total Phosphorus (µg/L)"
@@ -351,9 +385,11 @@ server <- function(input, output) {
     }
   }
 
-  # Graphs
+  # High-frequency data graphs
   output$HFPlot <- renderPlot({
     req(selectedHF())
+
+    # Heatmap
     if (input$HFGraphSelect == "Heatmap") {
       ggplot(selectedHF(), aes(x = date, y = meter, fill = value)) +
         geom_raster(interpolate = T) +
@@ -369,6 +405,7 @@ server <- function(input, output) {
           fill = "Temp (°C)"
         ) +
         theme_classic()
+    # DO plot
     } else if (input$HFGraphSelect == "DO at Depth") {
       ggplot(selectedHF(), aes(x = date, y = value, color = factor(meter), shape = factor(meter))) +
         geom_point(size = 2) +
@@ -383,6 +420,7 @@ server <- function(input, output) {
           color = "Meter"
         ) +
         theme_bw()
+    # Temperature plot
     } else if (input$HFGraphSelect == "Temperature at Depth") {
       ggplot(selectedHF(), aes(x = date, y = value, color = factor(meter), shape = factor(meter))) +
         geom_point(size = 2) +
@@ -403,6 +441,8 @@ server <- function(input, output) {
   #MS Plots
   output$MSPlot <- renderPlot({
     req(selectedMS())
+
+    # YSI temperature plot
     if (input$YSIGraphSelect == "Temperature") {
       ggplot(selectedMS(), aes(x = temp, y = meter, group = date, color = date)) +
         geom_path(size = 1) +
@@ -421,8 +461,8 @@ server <- function(input, output) {
           axis.title = element_text(size = 12),
           axis.text = element_text(size = 10)
         )
-    }
-    else if (input$YSIGraphSelect == "DO") {
+    #YSI DO plot
+    } else if (input$YSIGraphSelect == "DO") {
       ggplot(selectedMS(), aes(x = do, y = meter, group = date, color = date)) +
         geom_path(size = 1) +
         geom_point(size = 3) +
@@ -441,8 +481,8 @@ server <- function(input, output) {
           axis.title = element_text(size = 12),
           axis.text = element_text(size = 10)
         )
-    }
-    else if (input$YSIGraphSelect == "Both") {
+    # YSI temperature and DO plot
+    } else if (input$YSIGraphSelect == "Both") {
       ggplot(selectedMS(), aes(x = Value, y = meter, color = as.factor(date), group = interaction(date, Measurement))) +
         geom_path(size = 1) +
         geom_point(size = 3) +
@@ -461,11 +501,12 @@ server <- function(input, output) {
     }
   })
 
-  # Water Quality Graph 1 (Left)
+  # Water quality graph 1 (left)
   output$msTabSplitLeft <- renderPlot({
     req(input$msTab, input$leftWQSelect)
+
     if (input$msTab == "Water Quality") {
-      # Special case for Secchi Depth
+      # Special case for Secchi depth graph
       if (input$leftWQSelect == "Secchi Depth") {
         ggplot(WQSecchi, aes(x = date, y = secchi)) +
           geom_point(size = 4, color = "#1f908c") +
@@ -485,13 +526,15 @@ server <- function(input, output) {
             legend.position = "top"
           )
       } else {
-        # Select correct dataset
+        # Select correct dataset for non secchi graphs
         selectedData <- if (input$leftWQSelect == "Dissolved Organic Carbon") {
           WQDOC
         } else {
           WQ
         }
 
+        # Every graph that isn't secchi, y-axis is what the user selects
+        # Axis limits and labels are dynamically updated based on user selection
         ggplot(selectedData, aes(x = date, y = .data[[input$leftWQSelect]], color = site, shape = site)) +
           geom_point(size = 4) +
           scale_color_manual(values = c("EPI" = "yellow", "HYP" = "black")) +
@@ -519,11 +562,12 @@ server <- function(input, output) {
     }
   })
 
-  # Water Quality Graph 2 (Right)
+  # Water quality graph 2 (right)
   output$msTabSplitRight <- renderPlot({
     req(input$msTab, input$rightWQSelect)
+
     if (input$msTab == "Water Quality") {
-      # Special case for Secchi Depth
+      # Special case for Secchi depth graph
       if (input$rightWQSelect == "Secchi Depth") {
         ggplot(WQSecchi, aes(x = date, y = secchi)) +
           geom_point(size = 4, color = "#1f908c") +
@@ -543,13 +587,15 @@ server <- function(input, output) {
             legend.position = "top"
           )
       } else {
-        # Select correct dataset
+        # Select correct dataset for non secchi graphs
         selectedData <- if (input$rightWQSelect == "Dissolved Organic Carbon") {
           WQDOC
         } else {
           WQ
         }
 
+        # Every graph that isn't secchi, y-axis is what the user selects
+        # Axis limits and labels are dynamically updated based on user selection
         ggplot(selectedData, aes(x = date, y = .data[[input$rightWQSelect]], color = site, shape = site)) +
           geom_point(size = 4) +
           scale_color_manual(values = c("EPI" = "yellow", "HYP" = "black")) +
